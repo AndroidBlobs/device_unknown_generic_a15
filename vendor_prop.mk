afb9cfb852978cda444cf0f6a8a8fe80331ff819
ShivamKumarJha <jha.shivam3@gmail.com>
2019-12-15 16:25:27 +0100

    generic_a15-user 9 PPR1.180610.011 eng.root.20191211.100625 test-keys
    Signed-off-by: ShivamKumarJha <jha.shivam3@gmail.com>

---
+++ vendor_prop.mk
@@ -0,0 +1,7 @@
+PRODUCT_PROPERTY_OVERRIDES +=  \
+    9.1.0.6(0B12) \
+    ro.carrier=unknown \
+    ro.config.filterservice=false \
+    ro.dalvik.vm.native.bridge=0 \
+    ro.vendor.build.display.id=Chipset-chicago \
+    ro.vendor.vndk.version=26.0.0 \
